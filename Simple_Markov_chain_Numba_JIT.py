1c5051c2e01fbf7d70cdeabe890b7a25b6d0bbf8
chadyuktech <72742717+chadyuktech@users.noreply.github.com>
2020-11-03 06:08:47 -0500

    Moved np.array definition out of jit compile.

--- Simple_Markov_chain_Numba_JIT.py
+++ Simple_Markov_chain_Numba_JIT.py
@@ -1,17 +1,19 @@
 import numpy as np
 from numba import jit
-"""Simple Markov chain:
+import time
+"""Simple Markov chain with a Numba-compiled loop:
         Initial state: State 1
         Probability of transition:
             State 1 -> State 0: 0.2
             State 0 -> State 1: 0.1
 """
-n = 1_000_000_000
-@jit
-def markov_chain():
-    state = np.ones(n, dtype=np.int_)
-    trans = np.random.uniform(size=n)
+n = 100_000_000
 
+state = np.ones(n, dtype=np.int_)
+trans = np.random.uniform(size=n)
+
+@jit
+def markov_chain(state, trans):
     for i in range(n-1):
         if state[i]==0:
             if trans[i]<.1:
@@ -20,6 +22,20 @@ def markov_chain():
                 state[i+1]=0
         elif trans[i]<.2:
                 state[i+1]=0
-    return 1 - state.mean()
+    return state
 
-print(f"Time in State 0: {markov_chain():%}")
+def run():
+    start = time.time()
+    state = np.ones(n, dtype=np.int_)
+    trans = np.random.uniform(size=n)
+    s0 = 1 - markov_chain(state, trans).mean()
+    end = time.time()
+    t = end - start
+    return s0,t
+
+r = run()
+print(f"Time in State 0: {r[0]:.4%}. Calc time incl compile: {r[1]:.2}s")
+
+r = run()
+print(f"Time in State 0: {r[0]:.4%}. Calc time incl compile: {r[1]:.2}s")
+
